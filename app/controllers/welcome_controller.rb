f04dfe611094c5f04ac6cf9b23e4c67f6e335cb5
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-06 22:21:20 +0900

    show environment variables by GET /welcome/index.json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    respond_to do |format|
+      format.json{
+        render :json => Hash[ENV.map{|k,v| [k,v]}.sort_by{|k,v| k}]
+      }
+    end
   end
 end
