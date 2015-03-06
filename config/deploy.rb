7dd752150516401e91f282d52ef0153e563b5e39
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-06 22:33:32 +0900

    configured deployer

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.2.1'
 
 set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'https://github.com/minimum2scp/railstest01.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/railstest01'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,10 +23,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml .env}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -39,8 +39,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:legacy_restart'
     end
   end
 
@@ -55,4 +54,32 @@ namespace :deploy do
     end
   end
 
+  desc "upload .env"
+  task :"upload_dotenv" do
+    local_path = "config/deploy/#{fetch(:stage)}.env"
+    remote_path = File.expand_path(".env", shared_path)
+    on roles(:app) do |host|
+      info "upload #{local_path} to #{remote_path}"
+      dir = File.dirname(remote_path)
+      if test "[ ! -d #{dir} ]"
+        execute "mkdir -p #{dir}"
+      end
+      upload!(local_path, remote_path)
+    end
+  end
 end
+
+set :rbenv_type, :system # :system or :user
+set :rbenv_root, '/opt/rbenv'
+set :rbenv_ruby, '2.1.5'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+#set :bundle_flags, '--quiet'
+#set :bundle_path, nil
+#set :bundle_binstubs, nil
+
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+set :unicorn_config_path, -> { File.join(current_path, "config", "unicorn.rb") }
+
