5dc4053b296dec36803f1f7dc892ff6ec3465676
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-06 22:44:44 +0900

    configure production stage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,16 @@
+(ENV['SERVERS'] || "").split(',').each.with_index do |s, idx|
+  server s, user: 'debian', roles: (idx==0 ? %w{web app db} : %w{web app})
+end
+
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,7 +19,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
